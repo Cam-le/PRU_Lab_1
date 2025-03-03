9cffff83457787517e740a67f51888bf4e91ba3c
Thanh3Bien <thanhlv1793@gmail.com>
2025-03-03 09:28:06 +0700

    RollDicerRemake

--- Assets/Scripts/Minigame/DiceRoller.cs
+++ Assets/Scripts/Minigame/DiceRoller.cs
@@ -1,60 +1,64 @@
-using System.Collections;
+﻿using System.Collections;
+using TMPro;
 using UnityEngine;
 
 public class DiceRoller : MonoBehaviour
 {
     public int numberOfSides = 12;
     private int lastRoll;
+    public TextMeshProUGUI resultText;
 
     private Quaternion originalRotation;
 
     void Start()
     {
-        originalRotation = transform.rotation; 
+        originalRotation = transform.rotation;
+        resultText.text = "0";
     }
+
     public void RollDice()
     {
         lastRoll = Random.Range(1, numberOfSides + 1);
         Debug.Log("Result Roll Dice: " + lastRoll);
         AnimateDiceRoll();
+        
     }
 
     private void AnimateDiceRoll()
     {
-        float randomRotationX = Random.Range(0f, 360f);
-        float randomRotationY = Random.Range(0f, 360f);
-        float randomRotationZ = Random.Range(0f, 360f);
-
-        transform.Rotate(randomRotationX, randomRotationY, randomRotationZ);
         StartCoroutine(RollCoroutine());
     }
+    private void UpdateResultText()
+    {
+        if (resultText != null)
+        {
+            resultText.text = lastRoll.ToString(); // Cập nhật nội dung TextMeshPro
 
+        }
+    }
     private IEnumerator RollCoroutine()
     {
+
+        resultText.text = "";
         Vector3 originalPosition = transform.position;
-        Vector3 targetPosition = originalPosition + new Vector3(0, 0.5f, 0);
-
-        float duration = 0.5f;
+        float duration = 1f; // Tổng thời gian quay
         float elapsed = 0f;
-
+        float rotationSpeed = 0.01f;
+        // Quay như bánh xe
         while (elapsed < duration)
         {
-            transform.position = Vector3.Lerp(originalPosition, targetPosition, elapsed / duration);
+            // Tính toán góc quay
+            float rotationAmount = rotationSpeed * 360f * (elapsed / duration); // Quay 360 độ trong thời gian và giảm tốc độ
+            transform.Rotate(0, 0, rotationAmount); // Xoay quanh trục Z
+
             elapsed += Time.deltaTime;
             yield return null;
         }
 
-        elapsed = 0f;
-
-        while (elapsed < duration)
-        {
-            transform.position = Vector3.Lerp(targetPosition, originalPosition, elapsed / duration);
-            elapsed += Time.deltaTime;
-            yield return null;
-        }
-
+        // Đặt lại vị trí và góc ban đầu
         transform.position = originalPosition;
         transform.rotation = originalRotation;
+        UpdateResultText();
     }
 
     public int GetLastRoll()
