0d7f12f301c8c8160147772804ee0604b44fcbd9
phamvietduc0411 <phamvietduc0411@gmail.com>
2025-03-05 20:20:04 +0700

    add instruction screen

---
+++ Assets/Scripts/QuestionScene/Instruction.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class InstructionManager : MonoBehaviour
+{
+    [SerializeField] private GameObject instructionCanvas; // Kéo InstructionCanvas vào đây
+    [SerializeField] private GameObject questionGame; // Kéo QuestionGame vào đây
+
+    private void Start()
+    {
+        // Chỉ hiển thị màn hướng dẫn ban đầu
+        instructionCanvas.SetActive(true);
+        questionGame.SetActive(false);
+    }
+
+    public void StartGame()
+    {
+        // Ẩn màn hướng dẫn và hiện game
+        instructionCanvas.SetActive(false);
+        questionGame.SetActive(true);
+    }
+}
